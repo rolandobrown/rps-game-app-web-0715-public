4d06b0b4319861ca897636d1923cf56eba2526be
rolandobrown <rolando.brown@gmail.com>
2015-08-25 15:51:05 -0400

    all done

--- app.rb
+++ app.rb
@@ -2,12 +2,10 @@ require_relative 'config/environment'
 
 class GameApp < Sinatra::Base
   get '/rps/:play' do
-
-    # game = RPSGame.new((params['play']).to_sym)
-    #   @play = game.item
-    #   @computer_play = game.computer_play
-    #   @result = game.game_status
-
+    game = RPSGame.new((params['play']).to_sym)
+    @play = game.item
+    @computer_play = game.computer_play
+    @result = game.game_status
     erb :rps_game
   end
 end
