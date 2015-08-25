5ef39fea9f380506f3bae1bc9d4fe4b91ded0449
rolandobrown <rolando.brown@gmail.com>
2015-08-25 15:40:09 -0400

    all done

--- app.rb
+++ app.rb
@@ -2,7 +2,12 @@ require_relative 'config/environment'
 
 class GameApp < Sinatra::Base
   get '/rps/:play' do
-    # Your app code goes here
+
+    # game = RPSGame.new((params['play']).to_sym)
+    #   @play = game.item
+    #   @computer_play = game.computer_play
+    #   @result = game.game_status
+
     erb :rps_game
   end
 end
--- models/rps_game.rb
+++ models/rps_game.rb
@@ -1,2 +1,41 @@
 class RPSGame
+
+  VALID_PLAY_TYPES = [:rock, :paper, :scissors]
+
+  WINNING_PAIRS = {
+    rock: :scissors,
+    paper: :rock,
+    scissors: :paper
+  }
+
+ attr_reader :item
+
+  def initialize(type)
+    @type = type
+    raise PlayTypeError unless VALID_PLAY_TYPES.include?(@type)
+  end
+
+  def self.valid_play?(type)
+    !!VALID_PLAY_TYPES.include?(type)
+  end
+
+  def computer_play
+      VALID_PLAY_TYPES[rand(0..2)]
+  end
+
+  def won?
+    !!(WINNING_PAIRS[@type] == computer_play)
+  end
+
+  def tied?
+    !!(@type == computer_play)
+  end
+
+  def lost?
+    !(won? || tied?)
+  end
+
+end
+
+class PlayTypeError < StandardError
 end
